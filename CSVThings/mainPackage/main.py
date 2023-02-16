d1ec7eca8c8159703df03fa86edd19c3ad65f0ca
fishe2jo <fishe2jo@LCBLAB3120J-12.uccob.uc.edu>
2023-02-16 13:45:05 -0500

    Edited

--- CSVThings/mainPackage/main.py
+++ CSVThings/mainPackage/main.py
@@ -4,3 +4,9 @@ from utilsPackage.utils import *
 
 # Call the function and store what it returns 
 #  in a variable called data
+data = read_CSV_file()
+#print(data)
+#print(data[-1])
+# I want a set of all the collision types
+collisionTypes = set([row[6] for row in data])
+print(collisionTypes)
